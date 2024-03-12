4fb2fd26127a3742ce1ba30955ca826b39a98b15
Крылович Сергей Станиславович <siarhei.krylovich@softclub.by>
2024-03-12 17:18:47 +0300

    add MainPage

--- src/test/java/ui/MainPage.java
+++ src/test/java/ui/MainPage.java
@@ -1,4 +1,21 @@
 package ui;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
 public class MainPage {
+
+    SelenideElement logInBtn = $x("//a[contains(@class,'navbar__auth_login')]");
+
+    public MainPage openMainPage() {
+        open("https://stepik.org/");
+        return this;
+    }
+
+    public AuthPage clickOnLogIn() {
+        logInBtn.click();
+        return new AuthPage();
+    }
 }
